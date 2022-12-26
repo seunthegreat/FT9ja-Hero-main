1d0ef7ccf89da60c8e18b2a1f08ee994b1b631f7
samuel-abilawon <Sam@ATAfrica.org>
2022-12-25 23:11:42 -0800

    Responsive Benefits component config

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -2,7 +2,7 @@ import { features } from "../constants";
 import styles, {layout} from "../style";
 
 const BenefitCard = ({icon, title, content, index}) => (
-  <div className={`flex flex-row h-full p-6 rounded-[5px] mb-5 feature-card ${index === 1 || index === 2 ? 'bg-dimGreen' : 'bg-white border'}`}>
+  <div className={`flex flex-row h-full p-6 rounded-[5px] feature-card ${index === 1 || index === 2 ? 'bg-dimGreen' : 'bg-white border'}`}>
     <div className="flex-1 flex flex-col ml-3 items-center">
       <div className={`w-[64px] h-[64px] bg-white mb-4 rounded-full ${styles.flexCenter}`}>
         <img src={icon} alt="icon" className="w-[100%] object-contain" />
@@ -10,7 +10,7 @@ const BenefitCard = ({icon, title, content, index}) => (
       <h4 className="font-poppins font-semibold text-center text-black text-[18px] leading-[23px] mb-4">
         {title}
       </h4>
-      <p className="font-poppins font-normal text-center text-gray-500 text-[16px] leading-[24px] mb-1">
+      <p className="font-poppins font-normal text-center text-gray-500 text-xs leading-[24px] mb-1">
         {content}
       </p>
     </div>
@@ -19,7 +19,7 @@ const BenefitCard = ({icon, title, content, index}) => (
 );
 
 const Benefits = () => (
-  <section id="features" className={[layout.section, `sm:px-16 px-20`]}>
+  <section id="features" className={[layout.section]}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading}>Benefits of becoming an Affiliate</h2>
     </div>
--- src/style.js
+++ src/style.js
@@ -1,6 +1,7 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
-    heading: "font-poppins font-semibold xs:text-[38px] text-[30px] text-black xs:leading-[76.8px] w-full",
+    heading: `xs:mt-10 font-poppins font-semibold text-3xl text-black lg:text-left 
+      md:text-left sm:text-center ss:text-center xs:text-center xs:leading-[40px] w-full`,
     heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
     paragraph: "font-poppins font-normal text-gray-600 text-[18px] leading-[30.8px]",
     normal: "font-poppins font-normal text-dimWhite text-[15px] leading-[25px]",
@@ -17,13 +18,13 @@ const styles = {
   };
   
   export const layout = {
-    section: `flex md:flex-row flex-col ${styles.paddingY}`,
+    section: `flex md:flex-row flex-col ${styles.paddingY} px-10`,
     sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
   
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
     sectionItems: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative 
-      grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4`,
+      grid  grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 sm:grid-cols-2 ss:grid-cols-2 xs:grid-cols-1 gap-4`,
   
     sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
   };
